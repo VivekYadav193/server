1084e4dff9291143adf4685200eb0398fe39d460
VivekYadav193 <vivekash03@gmail.com>
2023-11-05 13:52:05 +0530

    commit

--- models/User.js
+++ models/User.js
@@ -1,39 +1,23 @@
 const mongoose = require("mongoose");
-
 const UserSchema = mongoose.Schema({
-  name: {
-    type: String,
-  },
-
-  email: {
-    type: String,
-  },
-
-  password: {
-    type: String,
-  },
-
+  name: {    type: String,},
+  email: {type: String, },
+  password: {type: String, },
   confirmPassword: {
     type: String,
-  },
-
-  date: {
+  },date: {
     type: Date,
     default: Date.now,
   },
-
   accountType: {
     type: String,
     enum: ["admin", "educator", "developer", "student"],
-    required: true,
-  },
-
+    required: true,},
   educator: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Educator",
     default: null,
   },
-
   student: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Student",
